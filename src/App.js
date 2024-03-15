ea1099c19ccbee4b09184ebd67c9eb6db87d31a2
sumi126 <imratuzzahansumi@gmail.com>
2024-03-15 11:28:44 +0600

    Add object numerical example and sty le

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const person = {
+    name:"Samsu",
+    job:'singer',
+    monthlySalary:25000
+  }
+  var style = {
+    color:'red',
+    backgroundColor:'green'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +18,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={{color:"cyan"}}>I can emmet language</p>
+        <h1 className="style">I am a new learner in reactjs</h1>
+        <p style={style}>I am Bangladeshi</p>
+        <h3>I am {person.name}</h3>
+        <p>I am a {person.job}</p>
+        <p>My yearly salary is about {person.monthlySalary*12}</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
