33344fcb05e3b7b033a763ec6c20736830582c97
Imratuzzahan Sumi <imratuzzahansumi@gmail.com>
2026-01-21 16:44:31 +0600

    Dynamic data pass in the component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   const person = {
     name:"Samsu",
@@ -11,6 +12,7 @@ function App() {
     color:'red',
     backgroundColor:'green'
   }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +26,9 @@ function App() {
         <h3>I am {person.name}</h3>
         <p>I am a {person.job}</p>
         <p>My yearly salary is about {person.monthlySalary*12}</p>
+        <Product product={products}> </Product>
 
+          
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -37,5 +41,28 @@ function App() {
     </div>
   );
 }
-
+const products =[
+  {name:"Photoshop",price:"$99.9"}
+]
+function Product(props){
+  const productStyle = {
+    border:"1px solid lightgray",
+    borderRadius:"5px",
+    backgroundColor:"gray",
+    height:"200px",
+    weight:"600px",
+    margin:"10px",
+    padding:"10px"
+  }
+  console.log("Hi",props)
+  return (
+    <div>
+        <div style={productStyle} >
+          
+          <p>Product Name:  {props.product[0].name}</p>
+          <p>Product Price: {props.product[0].price}</p>
+        </div>
+    </div>
+  )
+}
 export default App;
