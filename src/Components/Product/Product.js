2bffe0e66cd889801c2158fa7ad74a7c75742f84
Imratuzzahan Sumi <imratuzzahansumi@gmail.com>
2026-01-22 18:44:07 +0600

    add product component into the shop

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-    return (
-        <div>
-            {props.product.name}
-        </div>
-    );
+  // console.log(props);
+  const { img, name, seller, price, stock } = props.product;
+  return (
+    <div className="product">
+      <div className="product-img">
+        <img src={img} alt="" />
+      </div>
+      <div className="product-detail">
+        <h4 className="product-name">{name}</h4>
+        <br />
+        <p><small>by: {seller}</small></p>
+        <p>${price}</p>
+        <p><small>Only {stock} left in stock - Order soon</small></p>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
